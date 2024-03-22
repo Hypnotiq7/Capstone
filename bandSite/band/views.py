245c56651aa533665819d014b67b59b02dd3f535
Shaniel Subban <shaniel.subban@gmail.com>
2024-03-22 20:02:28 +0200

    Added docstrings to views.py

--- bandSite/band/views.py
+++ bandSite/band/views.py
@@ -3,6 +3,9 @@ from .models import Album, Event
 
 # Create your views here.
 def home(request):
+    """
+    View function to show the main home page. Takes you to home.html and makes you register or login.
+    """
     return render(request, 'registration/home.html')
 
 def login(request):
